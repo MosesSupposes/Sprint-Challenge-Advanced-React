50f90795eb837b80b2a9d49e6d26ce8a6f9a6fdb
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-06 08:58:41 -0700

    fetch and render player data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import PlayerList from './components/PlayerList'
+
 export default class App extends Component {
+  state = {
+    players: []
+  }
+  
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ players: res })
+    })
+  }
+  
   render() {
     return (
       <div className="App">
+        <PlayerList players={this.state.players} />
       </div>
     )
   }
