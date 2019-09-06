aef8fffd4ea0a4bf50a6eaf37fccdf894218500e
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-06 09:37:28 -0700

    test to ensure that player data is present

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import playerData from '../../data'
+
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+test('Player data is present', () => {
+  console.log('HERE', playerData.data[0])
+  expect(playerData.data[0].name).toBe("Alex Morgan")
+  expect(playerData.data[0].country).toBe("United States")
+  expect(playerData.data[0].searches).toBe(100)
+
+  expect(playerData.data[1].name).toBe("Megan Rapinoe")
+  expect(playerData.data[1].country).toBe("United States")
+  expect(playerData.data[1].searches).toBe(99)
+
+  expect(playerData.data[2].name).toBe("Marta")
+  expect(playerData.data[2].country).toBe("Brazil")
+  expect(playerData.data[2].searches).toBe(18)
+})
+
+
